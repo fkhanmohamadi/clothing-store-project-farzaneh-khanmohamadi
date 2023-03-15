23350d684a8e6d5474b83db81ffeed024287c5c0
fkhanmohamadi <f.kh_mohamadi@yahoo.com>
2023-03-15 22:14:35 +0330

    import tailwind header component and edit logo

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Headre from '../../layout/header'
 
 function HomeScreen() {
   return (
     <div>
-      <h1>Hi</h1>
+      <Headre></Headre>
     </div>
   )
 }
